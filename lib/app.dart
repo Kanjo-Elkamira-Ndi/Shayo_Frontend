9a3e7a59ac1ae1d467eb63d7adaf1cb9ca09135b
Kanjo-Elkamira-Ndi <kanjoel39@gmail.com>
2026-06-20 03:10:00 +0100

    Feature: Created all files for the entire application

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 // import 'core/constants/splash_screen.dart';
 // import 'core/constants/onboarding_screen.dart';
-// import 'features/auth/login_screen.dart';
-// import 'features/auth/forgot_password_screen.dart';
-// import 'features/auth/verification_screen.dart';
-// import 'features/auth/signup_screen.dart';
-// import 'features/auth/location_access_screen.dart';
-// import 'features/home/home_screen.dart';
-// import 'features/restaurants/food_screen.dart';
-// import 'features/restaurants/food_details_screen.dart';
-// import 'features/restaurants/restaurant_menu_screen.dart';
-// import 'features/cart/cart_screen.dart';
-import 'features/cart/edit_cart.dart';
+// import 'pages/auth/login_screen.dart';
+// import 'pages/auth/forgot_password_screen.dart';
+// import 'pages/auth/verification_screen.dart';
+// import 'pages/auth/signup_screen.dart';
+// import 'pages/auth/location_access_screen.dart';
+// import 'pages/home/home_screen.dart';
+// import 'pages/restaurants/food_screen.dart';
+// import 'pages/restaurants/food_details_screen.dart';
+// import 'pages/restaurants/restaurant_menu_screen.dart';
+// import 'pages/cart/cart_screen.dart';
+import 'pages/cart/edit_cart.dart';
 
 class ShayoApp extends StatelessWidget {
   const ShayoApp({super.key});
